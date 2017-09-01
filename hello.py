d59612f2cf832ad653fa19e8e403752a2d29077b
r2010shadow <r2010shadow@163.com>
2017-09-01 13:23:09 +0000

    modify

--- hello.py
+++ hello.py
@@ -20,6 +20,15 @@ def user(name):
    #return '<h2>Hi , %s! see you there</h2>' % name
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
+
 if __name__  == '__main__':
     manager.run()
     app.run(debug=True)
